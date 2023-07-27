3f9565a176cd76c43e25083c46f473e17977a5b0
thanhnn <thanhnn@mascom.vn>
2023-07-27 09:13:39 +0700

    .

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -62,7 +62,7 @@ export const authOptions: NextAuthOptions = {
 }
 
 export default NextAuth({
-  secret: env.NEXTAUTH_SECRET,
+  secret: 'dc244ed507da1803cb42073e071c28ab',
   adapter: PrismaAdapter(new PrismaClient()),
   providers,
   session: {
